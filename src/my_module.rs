f1357915ed00553f1ac47a9ed3bf1d7d36034513
NumberFour8 <lukas.pohanka@inina.net>
2022-11-02 16:08:53 +0100

    Template improvements & update

---
+++ src/my_module.rs
@@ -0,0 +1,27 @@
+
+/// Pure Rust function
+pub fn foo() -> i32 {
+    42
+}
+
+/// Unit tests of pure Rust code
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_foo() {
+        assert_eq!(42, foo());
+    }
+}
+
+/// Module for WASM wrappers of Rust code
+pub mod wasm {
+    use wasm_bindgen::prelude::*;
+
+    #[wasm_bindgen]
+    pub fn foo() -> i32 {
+        super::foo()
+    }
+}
+
